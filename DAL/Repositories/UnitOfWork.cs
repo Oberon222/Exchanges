85cdf4767cf6a453177c52ea25d380a997d3af94
Kost_ni27@student.itstep.org <Kost_ni27@student.itstep.org>
2021-04-10 23:14:31 +0300

    IRepository. IUnitOfWork

--- DAL/Repositories/UnitOfWork.cs
+++ DAL/Repositories/UnitOfWork.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace DAL
 {
-    public class UnitOfWork : IDisposable
+    public class UnitOfWork : IUnitOfWork
     {
         private ExchangeModel context = new ExchangeModel();
         private GenericRepository<Exchange> exchangeRepository;
 
-        public GenericRepository<Exchange> ExchangeRepos
+        public IRepository<Exchange> ExchangeRepos
         {
             get
             {
